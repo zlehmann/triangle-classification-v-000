bc5d4ea03fb15aed77f8471924c05f6485630f62
Zachary Lehmann <zlehmann@gmail.com>
2018-10-06 15:50:19 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,7 @@ class Triangle
       raise TriangleError
     end
 
-    if @side1 + @side2 < @side3 || @side2 + @side3 < @side1 || @side1 + @side3 < @side2
+    if @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2
       raise TriangleError
     else
       if @side1 == @side2 && @side1 == @side3
